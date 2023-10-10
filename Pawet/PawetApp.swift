b5cd890b975f6bd144d7ae322e24c9068255985a
amalabughazaleh91 <amal_adel91@hotmail.com>
2023-10-10 10:22:15 +0300

    Initial Commit

---
+++ Pawet/PawetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawetApp.swift
+//  Pawet
+//
+//  Created by Amal Abughazaleh on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PawetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
