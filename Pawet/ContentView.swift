7d3325b4a096f57bbdd5c1d7634f439561ac277d
amalabughazaleh91 <amal_adel91@hotmail.com>
2023-10-13 12:14:15 +0300

    friday commit

--- Pawet/ContentView.swift
+++ Pawet/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                 ScrollView(.horizontal) {
                     HStack {
                         
-                        card1(name: "karmanvet", categ: "all pets", image: "MK").frame(width: 160, height: 300)
+                        card1(name: "Karmanvet", categ: "all pets", image: "MK").frame(width: 160, height: 300)
                         
                         
                         
@@ -47,7 +47,7 @@ struct ContentView: View {
                         card1(name: "TRY-CITY", categ: "all pets", image: "TRICITY").frame(width: 170, height: 300)
                             .padding(.vertical, -100)
                         
-                        card1(name: "PeT Way", categ: "all pets", image: "PETWAY").frame(width: 155, height: 300)
+                        card1(name: "Pet Way", categ: "all pets", image: "PETWAY").frame(width: 155, height: 300)
                             .padding(.vertical, -100)
                         
                     }
@@ -66,16 +66,42 @@ struct ContentView: View {
                 ScrollView(.horizontal) {
                     HStack {
                         
-                        card1(name: "karmanvet", categ: "all pets", image: "MK").frame(width: 160, height: 300)
+                        card1(name: "Pet Care", categ: "all pets", image: "PETCARE").frame(width: 160, height: 300)
                         
                         
                         
                         
                         
-                        card1(name: "TRY-CITY", categ: "all pets", image: "TRICITY").frame(width: 170, height: 300)
+                        card1(name: "The Colony", categ: "all pets", image: "THECOLONY").frame(width: 170, height: 300)
                             .padding(.vertical, -100)
                         
-                        card1(name: "PeT Way", categ: "all pets", image: "PETWAY").frame(width: 155, height: 300)
+                        card1(name: "Pet Clinic", categ: "all pets", image: "PETCLINIC").frame(width: 155, height: 300)
+                        
+                    }
+                }
+                
+                Text("NARJIS")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 1.0, green: 0.049, blue: 0.351))
+                
+                    .padding()
+                
+                
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        
+                        card1(name: "Pet PAW", categ: "all pets", image: "PETPAW").frame(width: 160, height: 300)
+                        
+                        
+                        
+                        
+                        
+                        card1(name: "Fluffy Care", categ: "all pets", image: "FLUFFYCARE").frame(width: 170, height: 300)
+                            .padding(.vertical, -100)
+                        
+                        card1(name: "Vet Plus", categ: "all pets", image: "VETPLUS").frame(width: 155, height: 300)
                         
                     }
                 }
