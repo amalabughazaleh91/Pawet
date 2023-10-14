c29f1ebb67048606bb0baf222211ab83e135c499
elham34 <elham.alwadie@gmail.com>
2023-10-14 20:41:22 +0300

    commit elham

---
+++ Pawet/search.swift
@@ -0,0 +1,18 @@
+//
+//  search.swift
+//  Pawet
+//
+//  Created by Elham on 28/03/1445 AH.
+//
+
+import SwiftUI
+
+struct search: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    search()
+}
