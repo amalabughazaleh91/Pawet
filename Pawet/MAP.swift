b0e7facffb77c8a4f3d244bd3203f28cd5d6735c
razanalsaad <147040760+razanalsaad@users.noreply.github.com>
2023-10-15 09:25:52 +0300

    Razan3 commit

--- Pawet/MAP.swift
+++ Pawet/MAP.swift
@@ -1,15 +1,8 @@
-
-//
-//  ContentView.swift
-//  mapkit attempt 7
-//
-//  Created by razan on 27/03/1445 AH.
-//
-
 import SwiftUI
 import MapKit
 
 extension CLLocationCoordinate2D {
+   
    
    static let clinic = CLLocationCoordinate2D(
       latitude: 24.633333,
@@ -39,40 +32,77 @@ extension CLLocationCoordinate2D {
       latitude: 24.75421913716477,
       longitude:46.69905023199785)
    
+   static let clinic7 = CLLocationCoordinate2D(
+      latitude: 24.72328861046609,
+      longitude: 46.6717933683481 )
+   
+   static let clinic8 = CLLocationCoordinate2D(
+      latitude: 24.837576070256915,
+      longitude: 46.679885611541195)
+   
 }
 
-struct MAP: View {
+struct MAP : View {
    @State var ChooseView = 0
    
    @State private var searchText = "Search your area"
    
+   
    var body: some View {
       
-      VStack{ 
-         Map {
-            Marker("Karmanvet Clinic", systemImage: "pawprint.circle.fill"
-                   ,coordinate: .clinic)
-            
-            Marker("Tri-City Clinic", systemImage: "pawprint.circle.fill",coordinate: .clinic1)
-            
-            Marker("Pet Care Clinic", systemImage: "pawprint.circle.fill", coordinate: .clinic2)
-            
-            Marker("The Colony Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic3)
-            
-            Marker("Pet Way Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic4)
-            
-            Marker("Pet Paw Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic5)
-            
-            Marker("Fluffy Care", systemImage: "pawprint.circle.fill",coordinate: .clinic6)
-            
-         }
-         .mapStyle(.standard(elevation: .realistic))
-         .frame(width: 350, height: 600).cornerRadius(35)
+      VStack{ Text("Pawet Map")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(Color.pink)
+            .padding(35)
+         
+         
+         
+         
       }
       
+      
+      
+      
+      Map {
+         Marker("Karmanvet Clinic", systemImage: "pawprint.circle.fill"
+                ,coordinate: .clinic)
+         .tint(.pink)
+         
+         Marker("Tri-City Clinic", systemImage: "pawprint.circle.fill",coordinate: .clinic1)
+            .tint(.pink)
+         
+         Marker("Pet Care Clinic", systemImage: "pawprint.circle.fill", coordinate: .clinic2)
+            .tint(.pink)
+         
+         Marker("The Colony Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic3)
+            .tint(.pink)
+         
+         Marker("Pet Way Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic4)
+            .tint(.pink)
+         
+         Marker("Pet Paw Clinic",systemImage: "pawprint.circle.fill" ,coordinate: .clinic5)
+            .tint(.pink)
+         
+         Marker("Fluffy Care", systemImage: "pawprint.circle.fill",coordinate: .clinic6)
+            .tint(.pink)
+         
+         Marker("Pet Clinics", systemImage: "pawprint.circle.fill",coordinate: .clinic7)
+            .tint(.pink)
+         
+         Marker("Vet Plus", systemImage: "pawprint.circle.fill",coordinate: .clinic8)
+            .tint(.pink)
+      }
+      
+      
+      .mapStyle(.standard(elevation: .realistic))
+      .frame(width: 300, height: 570).cornerRadius(35)
    }
+   
 }
+
 
 #Preview {
    MAP()
 }
+
